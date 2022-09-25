737937cee7595a562666267c8a11e3edcb308266
Анна <marinaanna542@gmail.com>
2022-09-25 14:27:41 +0300

    Initial commit

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,7 +1,20 @@
+"use strict";
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
+  let arr = [];
+  if (a == 0) {
+    arr.push(-c / b);
+    return arr;
+  }
+  let det = b ** 2 - 4 * a * c;
+  if (det >= 0) {
+    if (det == 0) {
+      arr.push(-b / (2 * a));
+    } else {
+      arr.push((-b + Math.sqrt(det)) / (2 * a));
+      arr.push((-b - Math.sqrt(det)) / (2 * a));
+    }
+  }
+  return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
