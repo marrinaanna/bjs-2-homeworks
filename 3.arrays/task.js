be4ef5473b2ee1e3ccc215b10cae2c497a720d79
Анна <marinaanna542@gmail.com>
2022-10-01 16:15:23 +0300

    Task 2

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,7 @@
 function compareArrays(arr1, arr2) {
   let result;
 
-  result =((arr1.length==arr2.length) && (arr1.every((elem, index) => elem === arr2[index])));
+  result = ((arr1.length == arr2.length) && (arr1.every((elem, index) => elem === arr2[index])));
 
   return result;
 }
@@ -9,7 +9,7 @@ function compareArrays(arr1, arr2) {
 function advancedFilter(arr) {
   let resultArr;
 
-  // Ваш код
+  resultArr = arr.filter(elem => elem > 0).filter(elem => elem % 3 == 0).map(elem => elem * 10);
 
-  return resultArr; // array
+  return resultArr;
 }
