9c0e6b7302d233bb6fddc1c47f9b472ac7439005
Анна <marinaanna542@gmail.com>
2022-10-01 13:13:29 +0300

    Some fix

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,22 +1,24 @@
 "use strict";
 //task 1
 function getArrayParams(arr) {
-  let min, max, sum, avg, i;
-  min = 100;
-  max = -100;
+  let min, max, sum, avg;
+  min = Infinity;
+  max = -Infinity;
   sum = 0;
   avg = 0;
-  for (i=0; i < arr.length; i++) {
-    if (arr[i] > max) 
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > max) {
       max = arr[i];
-    
-    if (arr[i] < min) 
+    }
+
+    if (arr[i] < min) {
       min = arr[i];
-    
+    }
+
     sum += arr[i];
   }
   avg = Number((sum / arr.length).toFixed(2));
-  console.log(min,max,sum,avg);
+  console.log(min, max, sum, avg);
   return { min: min, max: max, avg: avg };
 }
 
@@ -32,8 +34,10 @@ function worker(arr) {
 function makeWork(arrOfArr, func) {
   let max = -Infinity;
   for (let i = 0; i < arrOfArr.length; i++) {
-    if (func(arrOfArr[i]) > max)
-      max = func(arrOfArr[i]);
+    let difference = func(arrOfArr[i]);
+    if (difference > max) {
+      max = difference;
+    }
   }
 
   return max;
@@ -44,10 +48,12 @@ function worker2(arr) {
   let max = -Infinity;
   let min = Infinity;
   for (let i = 0; i < arr.length; i++) {
-    if (max < arr[i])
+    if (max < arr[i]) {
       max = arr[i];
-    if (min > arr[i]);
+    }
+    if (min > arr[i]) {
       min = arr[i];
+    }
   }
   return Math.abs(max - min);
 }
